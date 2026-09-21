018cf61e230eb5e88519467f117c27679757eb78
PawarPrerana <pawarprerana3115@gmail.com>
2026-09-21 15:14:42 +0530

    ADDED : Vowel or Consonant Checker

---
+++ Vowel or Consonant Checker.py
@@ -0,0 +1,23 @@
+'''
+# PRACTICAL NO.: 2(3)
+# COURSE CODE: CA - 214 (Practical based on Python Programming)
+# CLASS: SYBCA (Semester-III) | Academic Year: 2026-2027
+# NAME: PAWAR PRERANA SATISH
+# TITLE: Vowel or Consonant Checker.
+'''
+#CODE-
+print("VOWEL & CONSONANTS")
+char=input("Enter a single character:")
+match char:
+	case "a":
+		print("Vowel")
+	case "e":
+		print("Vowel")
+	case "i":
+		print("Vowel")
+	case "o":
+		print("Vowel")
+	case "u":
+		print("Vowel")
+	case _:
+		print("consonants")
