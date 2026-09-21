15cd3293edbccf9bee67adb9859875b038aef238
PawarPrerana <pawarprerana3115@gmail.com>
2026-09-21 14:34:02 +0530

    ADDED : Even or Odd number check by if else

---
+++ Even or Odd Checker.py
@@ -0,0 +1,13 @@
+'''
+# PRACTICAL NO.: 2(1)
+# COURSE CODE: CA - 214 (Practical based on Python Programming)
+# CLASS: SYBCA (Semester-III) | Academic Year: 2026-2027
+# NAME: PAWAR PRERANA SATISH
+# TITLE: Even or Odd Checker.
+'''
+# CODE-
+num=int(input("enter the number:"))
+if num %2==0:
+	print("even number")
+else:
+	print("odd number")
