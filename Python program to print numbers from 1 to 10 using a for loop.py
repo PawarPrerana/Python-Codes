14df30d0d33fa75fb785b76d43a3d5a4d78eb8a3
PawarPrerana <pawarprerana3115@gmail.com>
2026-09-24 13:12:26 +0530

    ADDED : Python program to print numbers from 1 to 10 using a for loop

---
+++ Python program to print numbers from 1 to 10 using a for loop.py
@@ -0,0 +1,24 @@
+'''
+# PRACTICAL NO.: 3(1)
+# COURSE CODE: CA - 214 (Practical based on Python Programming)
+# CLASS: SYBCA (Semester-III) | Academic Year: 2026-2027
+# NAME: PAWAR PRERANA SATISH
+# TITLE: Write a Python program to print numbers from 1 to 10 using a for loop.
+'''
+
+#CODE-
+for i in range (1,11,1):
+      print (i)
+      
+'''OUTPUT-
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+'''
