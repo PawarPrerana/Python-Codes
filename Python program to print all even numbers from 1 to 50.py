ea62fb02ee4d4022d1e68b0b4eb317ac3d927109
PawarPrerana <pawarprerana3115@gmail.com>
2026-09-24 13:39:57 +0530

    ADDED : Python program to print all even numbers from 1 to 50

---
+++ Python program to print all even numbers from 1 to 50.py
@@ -0,0 +1,39 @@
+'''
+# PRACTICAL NO.: 3(3)
+# COURSE CODE: CA - 214 (Practical based on Python Programming)
+# CLASS: SYBCA (Semester-Ill) | Academic Year: 2026-2027
+# NAME: PAWAR PRERANA SATISH
+# TITLE: Write a Python program to print all even numbers from 1 to 50.
+'''
+
+#CODE-
+for i in range (2,51,2):
+	print(i)
+	
+'''OUTPUT-
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+22
+24
+26
+28
+30
+32
+34
+36
+38
+40
+42
+44
+46
+48
+50
+'''
