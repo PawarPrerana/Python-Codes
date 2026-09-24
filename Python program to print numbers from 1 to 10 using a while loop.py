20151968ac14f3a604f27a616a7088d269bb0265
PawarPrerana <pawarprerana3115@gmail.com>
2026-09-24 13:50:00 +0530

    ADDED : Python program to print numbers from 1 to 10 using a while loop

---
+++ Python program to print numbers from 1 to 10 using a while loop.py
@@ -0,0 +1,26 @@
+'''
+# PRACTICAL NO.: 3(7)
+# COURSE CODE: CA - 214 (Practical based on Python Programming)
+# CLASS: SYBCA (Semester-III) | Academic Year: 2026-2027
+# NAME: PAWAR PRERANA SATISH
+# TITLE: Write a Python program to print numbers from 1 to 10 using a while loop.
+'''
+
+#CODE-
+i=1
+while i<=10:
+	print(i)
+	i=i+1
+	
+'''OUTPUT-
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+'''
