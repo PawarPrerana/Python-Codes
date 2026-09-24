13dae92cc3f378698694cc9669aecce47fcc4ac1
PawarPrerana <pawarprerana3115@gmail.com>
2026-09-24 13:44:40 +0530

    ADDED : Python program to print the multiplication table of a number entered by the user

---
+++ Python program to print the multiplication table of a number entered by the user.py
@@ -0,0 +1,26 @@
+'''
+# PRACTICAL NO.: 3(5)
+# COURSE CODE: CA - 214 (Practical  based on Python Programming)
+# CLASS: SYBCA (Semester-III) | Academic Year: 2026-2027
+# NAME: PAWAR PRERANA SATISH
+# TITLE: Write a Python program to print the multiplication table of a number entered by the user.
+'''
+
+#CODE-
+number=int(input("Enter number:"))
+for i in range(1,11):
+	print(number*i)
+	
+'''OUTPUT-
+Enter number:13
+13
+26
+39
+52
+65
+78
+91
+104
+117
+130
+'''
