d971642b21092c3dfe5f2a8bf63a07ed13c44480
PawarPrerana <pawarprerana3115@gmail.com>
2026-09-24 13:42:18 +0530

    ADDED : Python program to print all odd numbers from 1 to 50

---
+++ Python program to print all odd numbers from 1 to 50.py
@@ -0,0 +1,39 @@
+'''
+# PRACTICAL NO.: 3(4)
+# COURSE CODE: CA - 214 (Practical based on Python Programming)
+# CLASS: SYBCA (Semester-III) | Academic Year: 2026-2027
+# NAME: PAWAR PRERANA SATISH
+# TITLE: Write a Python program to print all odd numbers from 1 to 50.
+'''
+
+#CODE-
+for i in range (1,51,2):
+	print(i)
+	
+'''OUTPUT-
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+21
+23
+25
+27
+29
+31
+33
+35
+37
+39
+41
+43
+45
+47
+49
+'''
